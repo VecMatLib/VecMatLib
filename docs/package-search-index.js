b811e948237dd93c9608c41d37cd8a4d952e3d17
Nico <nico.hex6@gmail.com>
2022-07-04 09:21:31 +0200

    Removed docs folder

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.hexagonnico.vecmat.matrix"},{"l":"io.github.hexagonnico.vecmat.vector"}];updateSearchResults();
